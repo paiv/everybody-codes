5ad8794200fa872e947f28c052d2b1db13245f04
Pavlo Ivashkov <pavels.code@gmail.com>
2024-12-08 19:19:09 +0200

    quest 15

--- 2024/q15.py
+++ 2024/q15.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-import heapq
 import io
 import string
 from collections import deque
 
 
-def display(grid, path):
+def display(grid, path=[], /, pois=None, color=None):
+    palette = ['37;40', '37;41', '30;42', '30;43', '37;44', '37;45']
+    c = palette[(color or 0) % len(palette)]
     coff = '\033[0m'
-    con = '\033[37;41m'
+    con = f'\033[{c}m'
+    xon = f'\033[37;41m'
     minx = min(x for y,x in grid)
     maxx = max(x for y,x in grid)
     miny = min(y for y,x in grid)
@@ -15,15 +17,14 @@ def display(grid, path):
     with io.StringIO() as so:
         for y in range(miny, maxy+1):
             for x in range(minx, maxx+1):
-                if (c := grid.get((y,x))) is not None:
-                    if (y,x) in path:
-                        print(con, end='', file=so)
-                        print(c, end='', file=so)
-                        print(coff, end='', file=so)
-                    else:
-                        print(c, end='', file=so)
+                c = grid.get((y,x), ' ')
+                if pois and (y,x) in pois:
+                    s = f'{xon}{c}{coff}'
+                elif (y,x) in path:
+                    s = f'{con}{c}{coff}'
                 else:
-                    print(' ', end='', file=so)
+                    s = c
+                print(s, end='', file=so)
             print(file=so)
         print(so.getvalue())
 
@@ -82,62 +83,6 @@ def part2(data):
     return ans
 
 
-def part3(data):
-    world = {(y,x):c for y,s in enumerate(data.strip().splitlines())
-        for x,c in enumerate(s)}
-    grid = {p:c for p,c in world.items() if c not in '#~'}
-    herbs = [p for p,c in grid.items() if c != '.']
-    start = next((y,x) for (y,x),c in grid.items() if y == 0)
-    goal = ''.join(sorted({grid[p] for p in herbs}))
-    neib = [(1,0), (-1,0), (0,1), (0,-1)]
-    pois = [start] + herbs
-    goali = (1 << len(goal)) - 1
-    pix = [0] + [1 << goal.index(grid[p]) for p in herbs]
-
-    def find_paths(start, goals):
-        fringe = deque([(0, start)])
-        seen = set()
-        todo = set(goals)
-        while fringe:
-            dist, pos = fringe.popleft()
-            if pos in todo:
-                yield goals.index(pos), dist
-                todo.discard(pos)
-                if not todo: break
-            if pos in seen: continue
-            seen.add(pos)
-            y,x = pos
-            for dy,dx in neib:
-                q = (y+dy, x+dx)
-                if q not in grid: continue
-                fringe.append((dist+1, q))
-
-    inf = float('inf')
-    pn = len(pois)
-    m = [[inf]*pn for _ in range(pn)]
-    for i,s in enumerate(pois):
-        for j,w in find_paths(s, pois):
-            m[i][j] = w
-            m[j][i] = w
-
-    fringe = [(0, 0, 0)]
-    seen = set()
-    while fringe:
-        dist, pos, bag = heapq.heappop(fringe)
-        if pos == 0 and bag == goali:
-            ans = dist
-            break
-        k = (pos, bag)
-        if k in seen: continue
-        seen.add(k)
-        for q in range(pn):
-            c = pix[q]
-            if c & bag == 0:
-                if q or (bag == goali):
-                    heapq.heappush(fringe, (dist + m[pos][q], q, bag|c))
-    return ans
-
-
 def unmask(v, m):
     n = (v + m).bit_length()
     f = ((1 << n) - 1) - m
@@ -151,7 +96,7 @@ def part3(data):
     start = next((y,x) for (y,x),c in grid.items() if y == 0)
     neib = [(1,0), (-1,0), (0,1), (0,-1)]
 
-    def walk(grid, seen, start):
+    def walk(grid, seen, start, color=1):
         wave = list()
         fringe = deque([start])
         while fringe:
@@ -166,11 +111,10 @@ def part3(data):
                         wave.append(q)
                     else:
                         fringe.append(q)
-
         res = 0
         ls = 0
         for w in wave:
-            t,l = walk(grid, seen, w)
+            t,l = walk(grid, seen, w, color=color+1)
             res += t
             ls |= unmask(l, grid[start])
 
